038538d53872b71da8382ba0286635cdce82865b
Juho Friman <juho.t.friman@gmail.com>
2020-12-01 20:07:33 +0200

    Added tests to assert correct solutions!
    :smart:

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,4 +1,5 @@
 const DATA_FILE: &str = "data/day_1_1.txt";
+const TARGET: u32 = 2020;
 
 pub fn solve_part_1(){
   println!("Solving day one - part one");
@@ -14,7 +15,7 @@ fn calculate_part_one() -> u32 {
   let numbers = prepare_data();
   for a in 0..numbers.len() {
     for b in a + 1..numbers.len() {
-      if numbers[a] + numbers[b] == 2020 {
+      if numbers[a] + numbers[b] == TARGET {
         return numbers[a] * numbers[b]
       }
     } 
@@ -27,7 +28,7 @@ fn calculate_part_two() -> u32 {
   for a in 0..numbers.len() {
     for b in a + 1..numbers.len() {
       for c in b + 1..numbers.len() {
-        if numbers[a] + numbers[b] + numbers[c] == 2020 {
+        if numbers[a] + numbers[b] + numbers[c] == TARGET {
           return numbers[a] * numbers[b] * numbers[c]
         }
       }
@@ -54,5 +55,13 @@ mod tests {
   fn assert_values_are_summable() {
     let values: Vec<u32> = prepare_data();
     assert!(values.iter().sum::<u32>() > 0);
+  }
+  #[test]
+  fn assert_part_one() {
+    assert_eq!(calculate_part_one(), 744475u32)
+  }
+  #[test]
+  fn assert_part_two() {
+    assert_eq!(calculate_part_two(), 70276940u32)
   }
 }
